f42e2447a32388ce2bc2eb7ff74e37263f1df572
Michael Baumer <meesterbaumer@mac.com>
2019-09-07 14:03:03 -0500

    Added some more code

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('hey');
+document.getElementById("enterName").style.display = "none";
+
+const displayEnterName = () => {
+    document.getElementById("enterName").style.display = "initial";
+};
+
+
+const printToDom = (divId, toPrint) => {
+
+    document.getElementById(divId).innerHTML = toPrint;
+
+}
+
+const cardBuilder = () => {
+
+    let domString = '';
+    domString =
+    `
+    
+    `;
+    printToDom("card", domString);
+}
+
+document.getElementById("sortButton").addEventListener('click', cardBuilder);
